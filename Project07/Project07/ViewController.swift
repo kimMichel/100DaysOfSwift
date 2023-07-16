c3d495f8aafee1432e8e6eb9a2662d43ca03b760
Michel Kim <kim98.michel@gmail.com>
2023-07-16 16:53:52 -0300

    Setting up and UITabBarController

---
+++ Project07/Project07/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  Project07
+//
+//  Created by Michel Jaejin Kim on 16/07/23.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+    
+    var petitions = [String]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return petitions.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.textLabel?.text = "Title goes here"
+        cell.detailTextLabel?.text = "Subtitle goes here"
+        return cell
+    }
+
+}
+
