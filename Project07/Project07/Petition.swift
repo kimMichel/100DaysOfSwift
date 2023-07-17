eb51b98ed1887ef09390cbc491b860224ad7bdcd
Michel Kim <kim98.michel@gmail.com>
2023-07-16 22:21:23 -0300

    Parsing JSON using the Codable Protocol

---
+++ Project07/Project07/Petition.swift
@@ -0,0 +1,14 @@
+//
+//  Petition.swift
+//  Project07
+//
+//  Created by Michel Jaejin Kim on 16/07/23.
+//
+
+import Foundation
+
+struct Petition: Codable {
+    var title: String
+    var body: String
+    var signatureCount: String
+}
