12672da569485bc5e688e62e8cc18e92878a034e
Michel Kim <kim98.michel@gmail.com>
2023-07-18 14:36:14 -0300

    UICollectionView cells

---
+++ Project10/Project10/PersonCell.swift
@@ -0,0 +1,16 @@
+//
+//  PersonCell.swift
+//  Project10
+//
+//  Created by Michel Jaejin Kim on 18/07/23.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var name: UILabel!
+    
+    
+}
