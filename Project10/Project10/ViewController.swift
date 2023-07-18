0775d9868c25b1a61131be9a6ddcd23b41b6c22e
Michel Kim <kim98.michel@gmail.com>
2023-07-18 14:25:22 -0300

    Add Project 10

---
+++ Project10/Project10/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project10
+//
+//  Created by Michel Jaejin Kim on 18/07/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
